b8fc8190a71e576292075e6ed209766248b132be
SashaIlchenko <sandra.ilchenko11@gmail.com>
2022-12-14 16:35:46 +0200

    done 2 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,22 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
+const containerGallery = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkUp(galleryItems);
+containerGallery.insertAdjacentHTML('beforeend', galleryMarkup);
 
+function createGalleryMarkUp(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+    }).join('');
+};
+new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250,
+    close: true
+});
 console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import VimeoPlayer from "@vimeo/player";
+import throttle from 'lodash.throttle';
+const iframe = document.querySelector('#vimeo-player');
+const player = new VimeoPlayer(iframe);
+const PAUSE_TIME = "videoplayer-current-time";
+player.on('timeupdate', throttle(onPlay, 1000));
+function onPlay(e) {
+    const timeValue = e.seconds;
+    const dataTime = localStorage.setItem(PAUSE_TIME, timeValue);
+    console.log(timeValue);
+}
+player.setCurrentTime(localStorage.getItem(PAUSE_TIME)).then(function (seconds) {
+}).catch(function (error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
 
+        default:
+            // some other error occurred
+            break;
+    }
+});
