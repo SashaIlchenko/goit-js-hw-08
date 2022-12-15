89eba89e1a5dee2f60f6e0ffcf755fea3eb08552
SashaIlchenko <sandra.ilchenko11@gmail.com>
2022-12-15 18:17:43 +0200

    done task 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,18 +6,18 @@ const PAUSE_TIME = "videoplayer-current-time";
 player.on('timeupdate', throttle(onPlay, 1000));
 function onPlay(e) {
     const timeValue = e.seconds;
-    const dataTime = localStorage.setItem(PAUSE_TIME, timeValue);
+    localStorage.setItem(PAUSE_TIME, timeValue);
     console.log(timeValue);
 }
 player.setCurrentTime(localStorage.getItem(PAUSE_TIME)).then(function (seconds) {
 }).catch(function (error) {
     switch (error.name) {
         case 'RangeError':
-            // the time was less than 0 or greater than the video’s duration
+
             break;
 
         default:
-            // some other error occurred
+
             break;
     }
 });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,27 @@
+import throttle from 'lodash.throttle';
+const form = document.querySelector('.feedback-form');
+const dataForm = {};
+const USER_FDBC = "feedback-form-state";
+console.dir(form)
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onInputType, 500));
 
+function onInputType(e) {
+    dataForm[e.target.name] = e.target.value;
+    localStorage.setItem(USER_FDBC, JSON.stringify(dataForm));
+};
+populateForm();
+
+function onFormSubmit(e) {
+    e.preventDefault();
+    localStorage.removeItem(USER_FDBC);
+    console.log(dataForm);
+    e.target.reset();
+};
+function populateForm() {
+    const savedData = JSON.parse(localStorage.getItem(USER_FDBC));
+    if (savedData) {
+        form[0].value = savedData.email;
+        form[1].textContent = savedData.message;
+    };
+};
